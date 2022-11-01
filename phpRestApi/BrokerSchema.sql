19732582d16dd864ddabbd175dfb3aeefa7cddb8
SirTitch <35763984+SirTitch@users.noreply.github.com>
2022-11-01 17:42:47 +0000

    Created Broker Schema and Dummy DATA: * Normalised into 3 objects and set up PK and FK fields. * Set up a joining fields for policies. * Quick update to remove password from DB config.

---
+++ phpRestApi/BrokerSchema.sql
@@ -0,0 +1,173 @@
+CREATE DATABASE `brokerDB`
+
+use `brokerDB`
+
+CREATE TABLE `customer` (
+  `customer_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `customer_address` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `policy` (
+  `policy_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `policy_type` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `policy_premium` double NOT NULL,
+  `insurer_name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `customer` bigint(20) UNIQUE FOREIGN KEY REFERENCES customer(customer_id),
+  PRIMARY KEY (`policy_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `client` (
+  `client_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `client_name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+  PRIMARY KEY (`policy_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*A joining table for the client policies*/
+CREATE TABLE `client_policies` (
+  `client_id`   bigint(20)        NOT NULL,
+  `policy_id`  bigint(20)         NOT NULL,
+  KEY         (client_id),
+  KEY         (policy_id),
+  FOREIGN KEY (client_id)  REFERENCES employees   (client_id)  ON DELETE CASCADE,
+  FOREIGN KEY (policy_id) REFERENCES departments (policy_id) ON DELETE CASCADE,
+  PRIMARY KEY (client_id, policy_id)
+)
+
+
+INSERT INTO customer 
+(  
+  customer_id,
+  customer_name,
+  customer_address,
+)  VALUES (
+  1,
+  "ABC Joinery",
+  "12 Ascott Street, Dundee"
+);
+
+INSERT INTO customer 
+(  
+  customer_id,
+  customer_name,
+  customer_address,
+) VALUES (
+  2,
+  "XYZ Plumbing",
+  "24 Fleet Street, Glasgow"
+);
+
+INSERT INTO customer 
+(  
+  customer_id,
+  customer_name,
+  customer_address,
+) VALUES (
+  3,
+  "Fast Taxis",
+  "324b Bank Street, Aberdeen"
+);
+
+INSERT INTO policy 
+(  
+  policy_id,
+  policy_type,
+  policy_premium,
+  insurer_name
+  customer
+ ) VALUES (
+  1,
+  "Public Liability",
+  123.87,
+  "Aviva",
+  1
+);
+
+INSERT INTO policy 
+(  
+  policy_id ,
+  policy_type,
+  policy_premium ,
+  insurer_name
+  customer
+) VALUES (
+  2,
+  "Public Liability",
+  2321.45,
+  "Allianz"
+  2
+);
+
+INSERT INTO policy (  
+  policy_id ,
+  policy_type,
+  policy_premium ,
+  insurer_name
+  customer
+) VALUES (
+  3,
+  "Motor Fleet",
+  59897.00,
+  "Aviva",
+  3
+);
+
+INSERT INTO policy (  
+  policy_id ,
+  policy_type,
+  policy_premium ,
+  insurer_name
+  customer
+) VALUES (
+  4,
+  "Public Liability",
+  6845.00,
+  "QBE"
+  3
+);
+
+INSERT INTO policy (  
+  client_id,
+  client_name,
+  policies,
+) VALUES (
+  1,
+  "Achme Broker Ltd"
+);
+
+INSERT INTO client_policies 
+(
+  client_id,
+  policy_id
+) VALUES (
+  1, 
+  1
+);
+
+INSERT INTO client_policies 
+(
+  client_id,
+  policy_id
+) VALUES (
+  1, 
+  2
+);
+
+INSERT INTO client_policies 
+(
+  client_id,
+  policy_id
+) VALUES (
+  1, 
+  3
+);
+
+INSERT INTO client_policies 
+(
+  client_id,
+  policy_id
+) VALUES (
+  1, 
+  4
+);
--- phpRestApi/inc/config.php
+++ phpRestApi/inc/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "Champ1onsirtitch");
+define("DB_PASSWORD", ""); //Will be the password the mySQL DB internal for now
 define("DB_DATABASE_NAME", "rest_api_demo");
