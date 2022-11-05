3e7231d46b1fe15ad6e120cf4b49570281ca1f9f
SirTitch <35763984+SirTitch@users.noreply.github.com>
2022-11-05 11:46:11 +0000

    Remove Password Again

--- phpRestApi/inc/config.php
+++ phpRestApi/inc/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "Champ1onsirtitch"); //Will be the password the mySQL DB internal for now
+define("DB_PASSWORD", ""); //Will be the password the mySQL DB internal for now
 define("DB_DATABASE_NAME", "brokerDB");
