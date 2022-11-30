a19f3881fcdf96d546637bc455945fbd795ba468
SirTitch <35763984+SirTitch@users.noreply.github.com>
2022-11-30 18:22:34 +0000

    Update Icon

--- vueJSFrontend/src/components/Header.vue
+++ vueJSFrontend/src/components/Header.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="header">
-        <img class=" lazyLoaded" src="https://brokerinsights.com/wp-content/uploads/2019/08/broker-logo.gif" data-src="https://brokerinsights.com/wp-content/uploads/2019/08/broker-logo.gif">
+        <img
+            src="https://www.pngkey.com/png/detail/231-2317080_email-databases-database-icon-white-png.png"
+            alt="Email - Databases - Database Icon White Png@pngkey.com"
+        />
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Header'
-    }
+export default {
+    name: 'Header',
+}
 </script>
